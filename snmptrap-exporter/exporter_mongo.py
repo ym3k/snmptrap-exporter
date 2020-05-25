c570d6782a5c51abd5790809ff417a4d93424663
yama3 <yama3@preferred.jp>
2020-05-25 10:08:27 +0900

    some fix

--- snmptrap-exporter/exporter_mongo.py
+++ snmptrap-exporter/exporter_mongo.py
@@ -1,6 +1,5 @@
-from prometheus_client import start_http_server, Summary, Gauge
+from prometheus_client import start_http_server, Gauge
 from pymongo import MongoClient
-# import random
 import time
 from datetime import datetime
 
@@ -8,7 +7,10 @@ DBHOST='mongo'
 DBPORT=27017
 DBNAME='fluentd'
 COLLECTIONNAME='snmptrap'
-DELAY = 60 # in sec
+FLUSH = 10 # in sec
+INTERVAL = 30 #
+
+labelkey = ['zone', 'hostname', 'interface', 'linkstate']
 
 # connect to MongoDB
 class MongoConnect(object):
@@ -17,80 +19,53 @@ class MongoConnect(object):
         client = MongoClient(dbhost, port)
         db = client[dbname]
         self.collection = db[COLLECTIONNAME]
-        self.zone_link = []
-        self.group_link_zone = { 
-            "$group" : { 
-                "_id" : { "zone": "$zone", "linkstate": "$linkstate"} ,
-                "count" : { "$sum" : 1  } 
-            }}
+        self.gauges = set()
+
     def current_period(self):
         now = time.time() 
         # now = time() // DELAY * DELAY
         self.to_datetime = datetime.fromtimestamp(now)
-        self.from_datetime = datetime.fromtimestamp(now - DELAY)
+        self.from_datetime = datetime.fromtimestamp(now - INTERVAL)
 
-    def last_state_change(self):
-        self.current_period()
-        period = { "$and": [ {"time": { "$gte": self.from_datetime }}, 
-                             {"time": { "$lt":  self.to_datetime   }} ] }
+    # def last_state_change(self):
+    #     self.current_period()
+    #     period = { "$and": [ {"time": { "$gte": self.from_datetime }}, 
+    #                          {"time": { "$lt":  self.to_datetime   }} ] }
         
-        lschanges = self.collection.aggregate([{ "$match": period}, self.group_link_zone])
-        return [i for i in lschanges]
+    #     lschanges = self.collection.aggregate([{ "$match": period}, self.group_link_zone])
+    #     return [i for i in lschanges]
 
     def last_state_change2(self):
         self.current_period()
         period = { "$and": [ {"time": { "$gte": self.from_datetime }}, 
                              {"time": { "$lt":  self.to_datetime   }} ] }
-        value = {key: 0 for key in self.zone_link}
+        value = {key: 0 for key in self.gauges}
         for i in self.collection.find(period):
-            key = "_".join([i['zone'], i['linkstate']])
+            key = "_".join([i[k] for k in labelkey])
             if key in value:
                 value[key] += 1
             else:
                 value[key] =  1
-                self.zone_link.append(key)
+                self.gauges.add(key)
         return value
 
 
-# Create a metric to track time spent and requests made.
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
-g = Gauge('switch_linkstate_change', 'counts of link-up and down', ['zone', 'linkstate'])
-
-
-# @g.track_inprogress()
-# def f():
-#     g.set(4.2)
-#     g.set_to_current_time()
+g = Gauge('switch_linkstate_change', 'counts of link-up and down', labelkey)
 
 co = MongoConnect()
 
 
-# def gauge_update(waittime):
-#     lschanges = co.last_state_change()
-#     for i in lschanges:
-#         (labels, count) = i.values()
-#         g.labels(labels['zone'], labels['linkstate']).set(count)
-#     time.sleep(waittime)
-
 def gauge_update(waittime):
     a = co.last_state_change2()
     for mykey, myvalue in a.items():
-        (zone, linkstate) = mykey.split("_")
-        g.labels(zone, linkstate).set(myvalue)
-        # g.labels('zoneA', mykey).set(myvalue)
+        (z, h, i, l) = mykey.split("_")
+        g.labels(z, h, i, l).set(myvalue)
     time.sleep(waittime)
-
-# Decorate function with metric.
-# @REQUEST_TIME.time()
-# def process_request(t):
-#     """A dummy function that takes some time."""
-#     time.sleep(t)
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
     start_http_server(8000)
     # Generate some requests.
     while True:
-        # process_request(random.random())
-        gauge_update(DELAY)
+        gauge_update(FLUSH)
